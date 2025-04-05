be04ce2fb2ff76f32d417e7a8020ef5baedc6785
sergio <sergiolopezantequera@gmail.com>
2025-04-05 04:41:47 +0200

    Create WhatsAppButton.jsx

---
+++ components/WhatsAppButton.jsx
@@ -0,0 +1,10 @@
+export default function WhatsAppButton() {
+  return (
+    <a
+      href="https://wa.me/34649002658"
+      className="fixed bottom-8 right-8 bg-green-500 p-4 rounded-full shadow-lg hover:bg-green-600 transition-all"
+    >
+      <img src="/whatsapp.svg" alt="WhatsApp" className="w-8 h-8" />
+    </a>
+  );
+}
