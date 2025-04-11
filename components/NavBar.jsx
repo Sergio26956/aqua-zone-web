17ddd1fbd00dc36f4cae36cb4c1b693544ef3bcb
sergio <sergiolopezantequera@gmail.com>
2025-04-11 17:22:36 +0200

    Update NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="bg-blue-800 text-white p-4">
+    <nav className="bg-blue-800 text-white p-4 fixed top-0 w-full z-10 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">AQUA-ZONE</h1>
-        <ul className="flex space-x-4">
+        <h1 className="text-2xl font-bold">AQUAZONE</h1>
+        <ul className="flex space-x-6">
           <li>
             <a href="/" className="hover:underline">Inicio</a>
           </li>
           <li>
-            <a href="/admin" className="hover:underline">Admin</a>
+            <a href="/about" className="hover:underline">Quiénes Somos</a>
+          </li>
+          <li>
+            <a href="/contact" className="hover:underline">Contacto</a>
           </li>
         </ul>
       </div>
