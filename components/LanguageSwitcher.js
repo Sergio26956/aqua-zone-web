4b1065e16b768a68970458a9fd11d11aceef9f7f
sergio <sergiolopezantequera@gmail.com>
2025-04-12 07:49:10 +0200

    Create LanguageSwitcher.js

---
+++ components/LanguageSwitcher.js
@@ -0,0 +1,15 @@
+export default function LanguageSwitcher() {
+  return (
+    <div className="flex space-x-2">
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">
+        Español
+      </button>
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">
+        Portugués
+      </button>
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">
+        Inglés
+      </button>
+    </div>
+  );
+}
