4ae3f14412effbc69c8371b6713639a0c35cab62
sergio <sergiolopezantequera@gmail.com>
2025-04-14 08:56:53 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,13 @@
-import NavBar from "../components/NavBar";
-import Footer from "../components/Footer";
+import NavBar from '../components/NavBar';
 
 export default function About() {
   return (
-    <div>
+    <>
       <NavBar />
-      <main className="container mx-auto p-8">
-        <h1 className="text-4xl font-bold text-blue-600 mb-6">Quiénes Somos</h1>
-        <p className="text-lg text-gray-700 mt-4">
-          En AQUA-ZONE, nos especializamos en ofrecer experiencias únicas en parques terrestres y flotantes. Nuestro objetivo es brindar diversión, seguridad y momentos inolvidables para todos nuestros visitantes.
-        </p>
+      <main>
+        <h1>Sobre Nosotros</h1>
+        <p>Bienvenido a Aquazone Waterpark, el mejor parque acuático.</p>
       </main>
-      <Footer />
-    </div>
+    </>
   );
 }
