908ec19ac535c5e650116af78eabe6e883962418
sergio <sergiolopezantequera@gmail.com>
2025-04-14 07:14:54 +0200

    Create AnimatedLogo.js

---
+++ components/AnimatedLogo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Html } from '@react-three/drei';
+
+function Logo() {
+  return (
+    <mesh>
+      {/* Logo en 3D */}
+      <boxGeometry args={[3, 1, 1]} />
+      <meshStandardMaterial color="orange" />
+      <Html position={[0, 1, 0]}>
+        <div style={{ color: 'white', fontSize: '20px', fontWeight: 'bold' }}>
+          AQUAZONE WATERPARK
+        </div>
+      </Html>
+    </mesh>
+  );
+}
+
+export default function AnimatedLogo() {
+  return (
+    <Canvas style={{ height: '300px' }}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[2, 5, 2]} />
+      <OrbitControls enableZoom={false} />
+      <Logo />
+    </Canvas>
+  );
+}
