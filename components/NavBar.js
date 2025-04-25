60777ee8e5935800983076543f94104a683d7bbe
sergio <sergiolopezantequera@gmail.com>
2025-04-25 05:49:27 +0200

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,26 +1,14 @@
-import Link from 'next/link';
-import styles from '../styles/NavBar.module.css';
-
-export default function NavBar() {
+export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navList}>
-        <li>
-          <Link href="/">
-            <a>Inicio</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>Sobre Nosotros</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <a>Contacto</a>
-          </Link>
-        </li>
-      </ul>
+    <nav className="bg-blue-600 text-white py-4 fixed w-full z-50">
+      <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Aqua Zone</h1>
+        <ul className="flex space-x-6">
+          <li><a href="#hero" className="hover:underline">Inicio</a></li>
+          <li><a href="#attractions" className="hover:underline">Atracciones</a></li>
+          <li><a href="#contact" className="hover:underline">Contacto</a></li>
+        </ul>
+      </div>
     </nav>
   );
 }
