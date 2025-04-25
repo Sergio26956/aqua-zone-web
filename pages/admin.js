acc29b0686052ab165ec3d482df7979554c05994
sergio <sergiolopezantequera@gmail.com>
2025-04-25 06:38:31 +0200

    Update admin.js

--- pages/admin.js
+++ pages/admin.js
@@ -1,11 +1,11 @@
 export default function Admin() {
   return (
     <div className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
+      <h1 className="text-3xl font-bold mb-6">Panel de Administración</h1>
       <div className="grid grid-cols-2 gap-4">
         <div className="bg-white p-4 rounded-lg shadow-md">
-          <h3>Contacto</h3>
-          <p>
+          <h3 className="text-xl font-semibold mb-2">Contacto</h3>
+          <p className="text-gray-600">
             <strong>Email:</strong> sergiolopezantequera@hotmail.com  
             <br />
             <strong>Tel:</strong> +34 649 002 658
