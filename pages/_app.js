3c48e622d933d98b0fb63395633d284f760873f1
sergio <sergiolopezantequera@gmail.com>
2025-04-26 18:59:59 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,39 @@
 import "../styles/globals.css";
 import WhatsAppButton from "../components/WhatsAppButton";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    primary: "#0070f3",
+    secondary: "#00b4d8",
+    background: "#f8f9fa",
+    text: "#343a40",
+  },
+};
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag("config", "GA_TRACKING_ID", {
+        page_path: url,
+      });
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       {/* Botón Flotante de WhatsApp */}
       <WhatsAppButton />
-    </>
+    </ThemeProvider>
   );
 }
 
