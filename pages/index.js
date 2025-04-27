83cf2c184749f78458cd3334c2d37303667ba785
sergio <sergiolopezantequera@gmail.com>
2025-04-27 19:41:25 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,39 @@
+import Head from "next/head";
+import VideoBackground from "../components/VideoBackground";
+import AnimatedLogo from "../components/AnimatedLogo";
+import SocialButtons from "../components/SocialButtons";
+import SocialMediaBar from "../components/SocialMediaBar";
 
+export default function Home() {
+  return (
+    <div className="relative h-screen overflow-hidden">
+      <Head>
+        <title>Aqua Zone - La experiencia más impresionante</title>
+        <meta
+          name="description"
+          content="Bienvenido a Aqua Zone, donde la innovación y el diseño se encuentran para crear una experiencia inolvidable."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Fondo de video */}
+      <VideoBackground />
+
+      {/* Logo animado */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <AnimatedLogo
+          imageUrl="/images/logo.webp"
+          altText="Aqua Zone Logo"
+          config={{ rotation: 270, duration: 3 }}
+          theme="auto"
+        />
+      </div>
+
+      {/* Botones flotantes de redes sociales */}
+      <SocialButtons />
+
+      {/* Barra lateral de redes sociales */}
+      <SocialMediaBar />
+    </div>
+  );
+}
