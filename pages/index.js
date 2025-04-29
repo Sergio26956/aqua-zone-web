b6d9442d2c14cb5147bece5a4d16268f2bad074d
sergio <sergiolopezantequera@gmail.com>
2025-04-29 06:04:39 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,35 @@
-import ContactForm from "../components/ContactForm";
+import Head from "next/head";
+import VideoBackground from "../components/VideoBackground";
+import AnimatedLogo from "../components/AnimatedLogo";
+import SocialButtons from "../components/SocialButtons";
+import SocialMediaBar from "../components/SocialMediaBar";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div className="relative h-screen overflow-x-hidden">
-      {/* Otros componentes como el fondo de video, logo y redes sociales */}
-      <div className="absolute bottom-0 w-full bg-gray-100 py-10">
-        <ContactForm />
-      </div>
+    <div className="relative">
+      <Head>
+        <title>Aqua Zone - Innovación y Diseño</title>
+        <meta
+          name="description"
+          content="Bienvenido a Aqua Zone, donde la innovación y el diseño crean experiencias inolvidables."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Fondo de video */}
+      <VideoBackground />
+
+      {/* Sección Hero */}
+      <Hero />
+
+      {/* Botones flotantes y barra de redes sociales */}
+      <SocialButtons />
+      <SocialMediaBar />
+
+      {/* Pie de página */}
+      <Footer />
     </div>
   );
 }
